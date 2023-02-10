c316497b98502ef33da7c7b83dd3792f954149b4
gabrielamancini <gabimancinir@gmail.com>
2023-02-10 15:49:39 -0500

    Added text to the Footer

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -9,16 +9,16 @@ const Footer = () => {
             <div className="container grid md:grid-cols-3 gap-10">
                 <div className='md:mx-auto'>
                     <h2 className='text-h2 mb-4'>Gabriela Mancini</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lectus mattis nunc aliquam tincidunt est non. Viverra nec eu.</p>
+                    <p>My passion for problem-solving and attention to detail has earned me a reputation as a reliable and skilled web developer, consistently delivering projects on time and to a high standard</p>
                 </div>
                 <div className='md:mx-auto'>
                     <h3 className='text-h3 mb-8'>Pages</h3>
-                    <InternalLinks className='grid grid-cols-1 leading-10'/>
+                    <InternalLinks className='grid grid-cols-1 leading-10' />
                     <ExternalLinks />
                 </div>
                 <div className='md:mx-auto'>
                     <h3 className='text-h3 mb-4'>Follow Me</h3>
-                    <IconLinks className='social-icons invert'/>
+                    <IconLinks className='social-icons invert' />
                     <h3 className='text-h3 mt-6'>Contact Me</h3>
                     <a href='mailto:gabimancinir@gmail.com'>gabimancinir@gmail.com</a>
                 </div>
