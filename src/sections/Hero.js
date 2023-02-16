91ed3da83ad5cd94d8278641d16c544da48a30ca
gabrielamancini <gabimancinir@gmail.com>
2023-02-16 13:07:23 -0500

    Fixed hero width

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
     return (
         <section className='bg-hero-pattern2 bg-no-repeat bg-hero2Position bg-contain'>
             <div className="container flex flex-col items-center lg:flex-row">
-                <div className=" flex flex-col text-center pt-16 lg:w-2/3 lg:justify-between lg:pr-28 lg:text-left">
+                <div className=" flex flex-col text-center pt-16 lg:w-7/12 lg:justify-between lg:pr-2 lg:text-left">
                     <h3 className='text-h3 mb-4 font-satoshiLight'> Hello, there</h3>
                     <h1 className="font-satoshiBlack text-h1Sm lg:text-h1 ">
                     My name is <span className='text-darkMagenta'> Gabriela Mancini</span>
@@ -15,7 +15,7 @@ const Hero = () => {
                     <h2 className="mt-4 mb-16 text-h3">Frontend Developer & Teacher</h2>
                     <Button href="#contactMe" children='Get in Touch' className='btn-primary md:mx-auto lg:mx-0'/>
                 </div>
-                <div className="flex justify-center bg-hero-pattern bg-contain bg-no-repeat bg-center lg:w-1/3 lg:self-end">
+                <div className="flex justify-center bg-hero-pattern bg-contain bg-no-repeat bg-center  lg:self-end">
                     <img src={HeroImg} alt="Me thinking" className='mt-10 max-w-heroImgSm lg:max-w-heroImg' />
                 </div>
             </div>
