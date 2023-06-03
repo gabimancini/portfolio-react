bbfd11dccee1336f629195b1dfb300faad242761
gabimancini <gabimancinir@gmail.com>
2023-06-02 20:00:26 -0400

    Add data

---
+++ src/data/dataLinks.js
@@ -0,0 +1,28 @@
+import CV from '../assets/GabrielaManciniCV.pdf';
+export const internalLinks = [
+    {
+        id: 1,
+        text: 'Home',
+        href: '/',
+        download: false
+    },
+    {
+        id: 2,
+        text: 'About',
+        href: '#about',
+        download: false
+    }
+    ,
+    {
+        id: 3,
+        text: 'Services',
+        href: '#services',
+        download: false
+    }, 
+    {
+        id:7,
+        text: 'Tutorials',
+        href:'/tutorials',
+        download: false
+    }
+]
