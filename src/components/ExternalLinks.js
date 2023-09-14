0ea0d8291775c539cf2cfacc104e88bb6d6eeab6
gabimancini <gabimancinir@gmail.com>
2023-09-14 09:54:19 -0400

    Add blog link

--- src/components/ExternalLinks.js
+++ src/components/ExternalLinks.js
@@ -5,7 +5,7 @@ const ExternallLinks = () => {
     return (
         <ul className="flex">
                     <Links key='14' children='My Company' href='https://devmagister.com/' download='false' target='_blank' />
- 
+                    <Links key='15' children='Blog' href='https://medium.com/@gabrielamancini' download='false' target='_blank' />
         </ul>
     )
 }
